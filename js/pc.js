c5127e2b6a24db06ab784262167ae29290744871
TaliSantos <Talisantos23@hotmail.com>
2013-02-09 14:32:59 +0000

    jornada actual design

--- js/pc.js
+++ js/pc.js
@@ -22,19 +22,15 @@ function Actual(Isla,Categoria,Competicion) {
 	$.getJSON("https://pcan-ivansantos.rhcloud.com/jornada_actuals.json?Isla=" + Isla +"&&Categoria=" + Categoria + "&&Competicion=" +Competicion  + "&callback=?", function(data) {
  	var actual
  	actual = data[0].Jornada;
+	document.getElementById('jornada').innerHTML= '<h2> ' + 'Jornada' + actual + '</h2>';
  	$.getJSON("https://pcan-ivansantos.rhcloud.com/calendarios.json?Isla=" + Isla +"&&Categoria=" + Categoria + "&&Competicion=" + Competicion + "&&Jornada=" + actual + "&callback=?", function(data) {
   		var items = [];
-  
 		for (var i=0;i<data.length;i++){
-  			$.each(data[i], function(key, val) {
-    			items.push('<li id="' + key + '">' + val + '</li>');
-	 		 });
-			 var todos = $('<ul/>', {
-  	  			 'class': 'my-new-list',
-   	 			 html: items.join('')
- 			})
+    			
+			 var todos = $('#jornada');
+			 todos.append("<tr><td>"+  data[i].Lucha  +  "</td><td>"  + data[i].Fecha + "</td><td>" + " "  + data[i].Hora  + "</td><td>" + data[i].Resultado + "</td></tr>")
+			 
   		}
-  		todos.appendTo('#images');
 	});
 
 });
