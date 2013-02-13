685be44bdcc1880db9dc55238930e5d1f089071a
TaliSantos <Talisantos23@hotmail.com>
2013-02-13 19:41:08 +0000

    calendario completo js

--- js/pc.js
+++ js/pc.js
@@ -1,17 +1,32 @@
-function Jornada(Isla,Categoria,Competicion,Jornada) {
+function completo(Isla,Categoria,Competicion) {
+	
+	$.getJSON("https://pcan-ivansantos.rhcloud.com/jornada_actuals.json?Isla=" + Isla +"&&Categoria=" + Categoria + "&&Competicion=" +Competicion  + "&callback=?", function(data) {
+ 	var actual
+ 	actual = data[0].Jornada;
+	
+ 	$.getJSON("https://pcan-ivansantos.rhcloud.com/calendariocompleto.json?Isla=" + Isla +"&&Categoria=" + Categoria + "&&Competicion=" + Competicion + "&callback=?", function(data) {
+		var jorn = 0;
+		var items = [];
+		for (var i=0;i<data.length;i++){
+			 var todos = $('.jornada');
+			    if (jorn != data[i].Jornada){
+					if (actual > data[i].Jornada ){
+						$(".jornada h2").css("color","#900");
+					}
+				 todos.append("<h2>" + "Jornada "+ data[i].Jornada + "</h2>")
+				 jorn = jorn +1;
+			 }
+			 todos.append("<tr><td>"+  data[i].Lucha  +  "</td><td>"  + data[i].Fecha + "</td><td>" + " "  + data[i].Hora  + "</td><td>" + data[i].Resultado + "</td></tr>")
+			 
+  		}
+	});
 
-$.getJSON("https://pcan-ivansantos.rhcloud.com/calendarios.json?Isla=" + Isla +"&&Categoria=" + Categoria + "&&Competicion=" + Competicion + "&&Jornada=" + Jornada + "&callback=?", function(data) {
-  var items = [];
-  $.each(data[0], function(key, val) {
-    items.push('<li id="' + key + '">' + val + '</li>');
-  });
-  $('<ul/>', {
-    'class': 'my-new-list',
-    html: items.join('')
-  }).appendTo('#images');
 });
 
 }
+
+
+
 
 
 
@@ -22,12 +37,12 @@ function Actual(Isla,Categoria,Competicion) {
 	$.getJSON("https://pcan-ivansantos.rhcloud.com/jornada_actuals.json?Isla=" + Isla +"&&Categoria=" + Categoria + "&&Competicion=" +Competicion  + "&callback=?", function(data) {
  	var actual
  	actual = data[0].Jornada;
-	document.getElementById('jornada').innerHTML= '<h2> ' + 'Jornada' + actual + '</h2>';
  	$.getJSON("https://pcan-ivansantos.rhcloud.com/calendarios.json?Isla=" + Isla +"&&Categoria=" + Categoria + "&&Competicion=" + Competicion + "&&Jornada=" + actual + "&callback=?", function(data) {
   		var items = [];
+		var todos = $('.jornada');
+		todos.append("<h2>" + "Jornada "+ actual + "</h2>")
 		for (var i=0;i<data.length;i++){
     			
-			 var todos = $('#jornada');
 			 todos.append("<tr><td>"+  data[i].Lucha  +  "</td><td>"  + data[i].Fecha + "</td><td>" + " "  + data[i].Hora  + "</td><td>" + data[i].Resultado + "</td></tr>")
 			 
   		}
